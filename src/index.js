a867491efa67645dbc5ce96d82682ff8a2a3ea62
Nicolas Dufreche <nicolas.dufreche@naturabuy.fr>
2018-02-06 16:44:02 +0100

    test

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('../index.html'));
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
