5e6ce1ef04f5a3c886421ad9fadd846140eaf4a6
Nicolas Dufreche <n.dufreche@gmail.com>
2018-04-17 14:10:22 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-app.get('/', function(req, res){
-  res.sendFile(path.resolve('../index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('index.html'));
 });
 
-io.on('connection', function(socket){
+app.get('/yolo', (req, res) => {
+    res.sendFile(path.resolve('index.html'));
+});
+
+io.on('connection', socket => {
   console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('subscribe', ({ path, user }) => {
+    socket.join(path);
+    console.log(`Subscribing to ${path}`);
+    io.to(path).emit('connecting', { path, user });
+  });
 });
 
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
